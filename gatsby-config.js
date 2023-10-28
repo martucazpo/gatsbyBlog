46a7e6e03ad218dee1d9f6a75c0f0d4b785d4f1b
marco <martucazpo@gmail.com>
2023-10-28 07:45:36 -0700

    add site metadata and metadata hook

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+const siteMetadata = {
+    title: `My Gatsby Blog`,
+    description: `This is my coding blog.`,
+  };
+
+
+module.exports = {
+    siteMetadata,
+    plugins: [
+      `gatsby-plugin-mdx`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: `${__dirname}/content`,
+          name: `content`,
+        },
+      },
+    ],
+  };
