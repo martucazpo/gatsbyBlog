63f1d1158c14e2fe239b3f6ea15e9f31badd1213
marco <martucazpo@gmail.com>
2023-10-28 11:43:27 -0700

    add header and layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,20 @@
 const siteMetadata = {
     title: `My Gatsby Blog`,
     description: `This is my coding blog.`,
-  };
+};
 
 
 module.exports = {
     siteMetadata,
     plugins: [
-      `gatsby-plugin-mdx`,
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/content`,
-          name: `content`,
+        `gatsby-plugin-theme-ui`,
+        `gatsby-plugin-mdx`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content`,
+                name: `content`,
+            },
         },
-      },
     ],
-  };
+};
